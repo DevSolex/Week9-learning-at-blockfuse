a47432575f31d6b5b86f42d959621649430bcf68
DevSolex <devsolex6@gmail.com>
2025-08-06 12:46:08 +0100

    initial commit

---
+++ try.py
@@ -0,0 +1,13 @@
+name = "solex"
+name2 = name 
+
+print(hex(id(name)))
+print(hex(id(name2)))
+print(-21 % 2)
+
+names_of_jos_boys = {'solex': 'jos', 'solo': 'bukuru'}
+List = []
+List.append(names_of_jos_boys.keys())
+print(type(List))
+print(list(names_of_jos_boys.keys()))
+print(type([names_of_jos_boys.keys()]))
